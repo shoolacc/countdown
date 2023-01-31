162394565c21e0184948ed1f36249d63a810edb2
shoolacc <123706564+shoolacc@users.noreply.github.com>
2023-01-31 11:22:20 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.showNumber(5)
+basic.pause(1000)
+basic.showNumber(4)
+basic.pause(1000)
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(1)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
